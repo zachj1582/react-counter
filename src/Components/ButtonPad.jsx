0e1c322d7677c07b02376d35fda6ec1264d8c783
Zachary Johnson <zach_j@att.net>
2020-06-01 18:07:27 -0500

    most of the functionality done, need to finish history and styling

--- src/Components/ButtonPad.jsx
+++ src/Components/ButtonPad.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
 
-export default function ButtonPad() {
-    return (
-        <div>
-            <button>Up Button</button>
-            <button>Down Button</button>
-        </div>
-    )
+export default function ButtonPad(props) {
+
+
+  return (
+    <div>
+      <button onClick={()=>props.inc1()}>+1</button>
+      <button onClick={()=>props.dec1()}>-1</button>
+      <button onClick={()=>props.inc2()}>+2</button>
+      <button onClick={()=>props.inc5()}>+5</button>
+      <button onClick={()=>props.inc10()}>+10</button>
+      <button onClick={()=>props.reset()}>Reset</button>
+    </div>
+  );
 }
--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,16 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import ButtonPad from "./ButtonPad";
 
 function Display(props) {
-    // const [display,setDisplay] = useState(0);
+  let [display, setDisplay] = useState(0);
+  let [countHist, setCountHist] = useState([]);
 
-    return (
-        <section className="display">
-            This is the Display for the Counter.
-        </section>
-    )
+  const increment1 = () => {
+    setDisplay((display += 1));
+  };
 
+  const increment2 = () => {
+    setDisplay((display += 2));
+  };
 
+  const increment5 = () => {
+    setDisplay((display += 5));
+  };
+
+  const increment10 = () => {
+    setDisplay((display += 10));
+  };
+
+  const decrement1 = () => {
+    setDisplay((display -= 1));
+  };
+
+  const reset = () => {
+    setCountHist((countHist) => [...countHist, display]);
+    setDisplay((display = 0));
+  };
+
+  return (
+      <section className="display">
+      {display}
+      <ButtonPad
+          inc1={increment1}
+          inc2={increment2}
+          inc5={increment5}
+          inc10={increment10}
+          dec1={decrement1}
+          reset={reset}
+        />
+        {countHist}
+    </section>
+  );
 }
 
 export default Display;
-
